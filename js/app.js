df9475822d358efd065b51601173beeb8687065a
suark <brian@granduke.ca>
2017-02-07 14:09:58 -0700

    Fixed issue that was caused by lack of required semi-colon.

--- js/app.js
+++ js/app.js
@@ -2,19 +2,6 @@
 
 var [d, m, y] = [15, 7, 2015]
 
-[m, y] = [y, m]
+;[m, y] = [y, m]
 
 console.log(m, y)
-
-var a = 1;
-var b = 3;
-
-[a, b] = [b, a];
-console.log(a); // 3
-console.log(b); // 1
-
-// let [a, b, c] = [1, 2, 3]
-// console.log(a, b, c)
-//
-// [b, c] = [c, b]
-// console.log(a, b, c)
