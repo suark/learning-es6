e548748045379b3a47638a40e42ff7a8b9f1b2fe
Brian Kraus <suark.nairb@gmail.com>
2017-02-10 16:30:23 -0700

    just starting lesson on sets

--- js/app.js
+++ js/app.js
@@ -1,19 +1,5 @@
-//generators
-/*
-A special kind of function that returns an iterator
+"use strict";
 
-They stop execution after each "yield" until next is called again
-*/
+//Sets
 
-//the * makes it a generator function
-function* logMessages() {
-  yield 'sup homies'
-  yield 'sup bromies'
-  yield 'sup yomies'
-}
-
-let msg = logMessages()
-
-for (let word of msg) {
-  console.log(word)
-}
+let s = new Set
