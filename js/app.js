0fbf0d0ac56042f14cb4a39472f0331d88c698ab
Brian Kraus <suark.nairb@gmail.com>
2017-02-10 12:18:00 -0700

    destructuring objects

--- js/app.js
+++ js/app.js
@@ -1,24 +1,14 @@
-console.log('array destructuring')
+"use strict";
+//destructuring objects
 
-let numbers  = [10, 20, 30, 40, 50]
-
-let [first, second, third, , fifth] = numbers
-
-console.log(fifth)
-
-function arrayDestructuring([first, second, ...rest]) {
-  console.log(first)
-  console.log(second)
-  console.log(rest)
+let course = {
+  name: 'Dutch Ovens For Life',
+  publisher: 'WootWoot'
 }
 
-arrayDestructuring(numbers)
+function courseDetails(c) {
+  let {name, publisher, ?price} = c
+  console.log(name, publisher)
+}
 
-console.log('destructuring nested arrays')
-
-let numbers_nested = [10, 20, [30, 40, 50]]
-let [first_n, second_n, [third_n, , fifth_n]] = numbers_nested
-
-console.log(first_n)
-console.log(second_n)
-console.log(fifth_n)
+courseDetails(course)
