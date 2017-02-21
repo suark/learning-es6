ed9c7763d59d7894cccb74b76861cabf36bc26a5
Brian Kraus <suark.nairb@gmail.com>
2017-02-21 16:37:41 -0700

    template strings

--- js/app.js
+++ js/app.js
@@ -1,33 +1,17 @@
 "use strict";
 
-//weakmaps
+//template strings
+let name = 'Brian'
+let tag = 'Hey, what is up'
 
-//a collection of keys and values, but the key must be an object
-let weak = new WeakMap()
+let s = `name is ${name}, tag is ${tag}`
 
-// weak.set('publisher', 'skillbakery')
-// console.log(weak.get('publisher'))
-//These get an error because a key must be an object
+console.log(s)
 
-let course = {name: 'Things', publisher:'skillbakery'}
-let company = {name: 'companyname'}
+//can also do multiline strings
 
-weak.set(course, {price: 59})
-weak.set(company, 'SB')
+let multi = `this thing
+is \`on
+multiple lines`
 
-console.log(weak.get(course))
-console.log(weak.get(company))
-
-//Other functions
-console.log(weak.has(course))
-weak.delete(course)
-console.log(weak)
-//Weakmap's .clear function was removed so it won't work anymore
-// weak.clear(company)
-// console.log(weak)
-//Now if we change the company object
-// company = null
-// console.log(weak.get(company))
-//We can no longer access the associated value
-
-//Weakmap has no size. It is an unorderd collection of objects and values
+console.log(multi)
