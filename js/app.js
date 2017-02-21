6be8d9b77dfdf2c75f69c1d6c740c64de4380ae5
Brian Kraus <suark.nairb@gmail.com>
2017-02-21 16:57:45 -0700

    es6 promise fetch method

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
 "use strict";
 
-//template strings
-let name = 'Brian'
-let tag = 'Hey, what is up'
+//fetch method
 
-let s = `name is ${name}, tag is ${tag}`
+fetch('http://services.odata.org/V4/Northwind/Northwind.svc/', { //1
+  method: 'get'
+}).then(function(response) { //2
+  return response.json()
+}).then(function(data) {
+  console.log(data.value) //3
+}).catch(function() {
+  console.log('Failed to fetch data') //4
+})
 
-console.log(s)
-
-//can also do multiline strings
-
-let multi = `this thing
-is \`on
-multiple lines`
-
-console.log(multi)
+//The first part returns a promise
+//The second part returns another promise that we pass to the next function
+//the third part gets the promise and displays the passed data
+//If an error occurrs, it ends up at the catch
