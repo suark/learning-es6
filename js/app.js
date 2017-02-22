fb3fa83a55c87545fb3ca357b8526164fc8baa64
Brian Kraus <suark.nairb@gmail.com>
2017-02-21 17:16:51 -0700

    es6 more on promises

--- js/app.js
+++ js/app.js
@@ -1,18 +1,48 @@
 "use strict";
 
-//fetch method
+//more on promises
 
-fetch('http://services.odata.org/V4/Northwind/Northwind.svc/', { //1
-  method: 'get'
-}).then(function(response) { //2
-  return response.json()
-}).then(function(data) {
-  console.log(data.value) //3
-}).catch(function() {
-  console.log('Failed to fetch data') //4
+//constructor of a promise
+let promise = new Promise(function(resolve, reject) {
+  if (success) {
+    resolve(data)
+  } else {
+    reject('Error')
+  }
 })
 
-//The first part returns a promise
-//The second part returns another promise that we pass to the next function
-//the third part gets the promise and displays the passed data
-//If an error occurrs, it ends up at the catch
+//defining the functions to be used as your resolve and reject for the promise
+promise.then(function(result) {
+  console.log(result)
+}, function(err) {
+  console.log(err)
+})
+
+//other functions of promise
+
+//This function will only be called when all the promises passed in, return
+//this is freaking awesome
+Promise.all([promise])
+//how to use it
+Promise.all([promise]).then(function(results){
+  //do something
+})
+
+//Race function will run when at least one of the promises has completed
+Promise.race([promise]).then(function(results){
+  //do something
+})
+
+//Refect function will return a promise that is rejected
+Promise.reject(new Error('fail')).then(function(error) {
+  // not called
+}, function(error) {
+  console.log(error); // Stacktrace
+});
+
+//The resolve function will return a promise that is resolved
+Promise.resolve('Success').then(function(value) {
+  console.log(value); // "Success"
+}, function(value) {
+  // not called
+});
