c3f0e63a8f981da8312333883c39bf91b2120989
Brian Kraus <suark.nairb@gmail.com>
2017-02-23 16:04:24 -0700

    object proxy

--- app/index.js
+++ app/index.js
@@ -1,7 +1,26 @@
-// New literals
-// Binary and Octal
+// Proxy
+/*
+Proxy
+Params:
+  Target: Object or function to wrap with Proxy.
+  Handler: Object whose properties are functions which define the behavior of
+  the proxy when an operation is performed on it.
+*/
 
-console.log(parseInt('00110010', 2))
-console.log(0b00110010) // binary
-console.log(parseInt('062', 8))
-console.log(0o062) // octal
+let course = new Proxy({}, {
+  // Handler callbacks
+  get: (target, name, receiver) => {
+    console.log('Property', name)
+    return name
+  },
+  set: (target, name, value, receiver) => {
+    console.log('Assignment on Property', name)
+    return value
+  },
+})
+
+course.name = 'Brian'
+course.publisher = 'Stuff'
+
+console.log(course.name)
+console.log(course.publisher)
