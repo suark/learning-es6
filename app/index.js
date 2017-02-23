75ad299dac77f8bb2d865f567ca471e7894c29d6
Brian Kraus <suark.nairb@gmail.com>
2017-02-23 14:45:34 -0700

    New number methods

--- app/index.js
+++ app/index.js
@@ -1,14 +1,25 @@
-// New String Methods
+// New Number Methods
+console.log('---Number Truncation---')
+console.log(Math.trunc(39.7))
+console.log(Math.trunc(0.5))
+console.log(Math.trunc(-0.3))
 
-console.log('Hello World'.repeat(2))
+console.log('---Number Type Checking---')
+console.log(Number.isNaN(45))
+console.log(Number.isNaN(NaN))
 
-console.log('World'.includes('rl'))
+console.log(Number.isFinite(Infinity))
+console.log(Number.isFinite(-Infinity))
+console.log(Number.isFinite(NaN))
+console.log(Number.isFinite(156))
 
-console.log('World'.startsWith('Wo'))
-console.log('World'.endsWith('d'))
+console.log('---Sign Determination---')
+console.log(Math.sign(10))
+console.log(Math.sign(0))
+console.log(Math.sign(-0))
+console.log(Math.sign(-10))
+console.log(Math.sign(NaN))
 
-console.log('\uD83D\uDE80')
-
-for (let ch of 'World') {
-  console.log(ch)
-}
+console.log('---Number Safety Checking---')
+console.log(Number.isSafeInteger(42))
+console.log(Number.isSafeInteger(90081992547408822))
